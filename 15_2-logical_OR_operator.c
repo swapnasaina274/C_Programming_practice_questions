833a6517f8866d32aded4aadf3975c08044097b3
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-02 20:58:00 +0530

    Add files via upload

---
+++ 15_2-logical_OR_operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+     printf("Enter the value of a and b:");
+    scanf("%d %d", &a, &b);
+    if(a||b)
+    {
+        printf("TRUE\n");
+    }
+    else
+    {
+        printf("FALSE\n");
+    }
+    return 0;
+}
