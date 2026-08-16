c61311637c83af36673813246e53214a303364a2
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-16 12:02:06 +0530

    Delete 9_3-multiplication.c

--- 9_3-multiplication.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    printf("Enter two numbers:");
-    scanf("%d %d", &a,&b);
-    int mul=a*b;
-    printf("The multiplication of a and b is: %d",mul);
-    return 0;
-}
