f67b3bef9a98e47e9a2867a26a086d40f6bc896e
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-16 12:04:30 +0530

    Delete 9_1-addition.c

--- 9_1-addition.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    printf("Enter two numbers:");
-    scanf("%d %d", &a,&b);
-    int sum=a+b;
-    printf("The addition of a and b is: %d",sum);
-    return 0;
-}
