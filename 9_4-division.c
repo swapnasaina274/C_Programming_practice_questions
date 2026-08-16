041ad584e70a93d130d1ff74a2fcc4a528638e34
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-16 12:01:54 +0530

    Delete 9_4-division.c

--- 9_4-division.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    printf("Enter two numbers:");
-    scanf("%d %d", &a,&b);
-    int div=a/b;
-    printf("The division of a and b is: %d",div);
-    return 0;
-}
