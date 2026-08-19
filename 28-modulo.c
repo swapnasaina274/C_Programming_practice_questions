3e1e8ad09aba449c89f0e32baca6656ad19ca622
swapnasaina274 <sainaswapna274@gmail.com>
2026-08-19 10:07:39 +0530

    Delete 28-modulo.c

--- 28-modulo.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    printf("Enter two numbers:");
-    scanf("%d %d", &a,&b);
-    int modulo=a%b;
-    printf("The remainder is: %d",modulo);
-    return 0;
-}
